c0c206d5ab073369e3754f9ae02a2d339c249045
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 19:06:15 -0300

    feat: adiciona notaController

---
+++ src/main/java/com/ctw/escola/controller/NotaController.java
@@ -0,0 +1,77 @@
+package com.ctw.escola.controller;
+
+
+import com.ctw.escola.dto.nota.NotaRequestDto;
+import com.ctw.escola.dto.nota.NotaResponseDto;
+import com.ctw.escola.service.NotaService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/notas")
+public class NotaController {
+
+    private final NotaService notaService;
+
+    public NotaController(NotaService notaService) {
+        this.notaService = notaService;
+    }
+
+    @PostMapping
+    public NotaResponseDto putNotas(
+            @RequestBody NotaRequestDto notaRequestDto
+    ){
+        try {
+            return notaService.postNota(notaRequestDto);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public List<NotaResponseDto> getNotas(){
+        try {
+            return notaService.getNotas();
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping("/{id}")
+    public NotaResponseDto getNotaPorId(
+            @PathVariable long id
+    ){
+        try {
+            return notaService.getNotasPorID(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public NotaResponseDto putNota(
+            @RequestBody NotaRequestDto notaRequestDto,
+            @PathVariable long id
+    ){
+        try {
+            return notaService.putNotas(notaRequestDto,id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteNota(
+            @PathVariable long id
+    ){
+        try {
+            notaService.deleteNota(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+
+}
