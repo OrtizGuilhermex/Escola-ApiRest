a8e46b3c416d03a65e7d58d48de5fef154667327
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 14:08:25 -0300

    feat: Adiciona Conexao com Banco de dados

---
+++ src/main/java/com/ctw/escola/utils/Conexao.java
@@ -0,0 +1,16 @@
+package com.ctw.escola.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexao {
+
+    private static final String URL = "jdbc:mysql://localhost:3356/escola?useSSL=false&serverTimezone=UTC";
+    private static final String USER = "root";
+    private static final String PASSWD = "mysqlPW";
+
+    public static Connection conectar() throws SQLException {
+        return DriverManager.getConnection(URL,USER,PASSWD);
+    }
+}
