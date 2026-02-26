997727170c4da48fb86727d4fba20476254b8a45
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 14:23:19 -0300

    feat: Adiciona TurmaRequestDto/ResponseDto

---
+++ src/main/java/com/ctw/escola/dto/turma/TurmaRequestDto.java
@@ -0,0 +1,13 @@
+package com.ctw.escola.dto.turma;
+
+import com.ctw.escola.model.Aluno;
+
+import java.util.List;
+
+public record TurmaRequestDto (
+        String nome,
+        long curso_id,
+        long professor_id,
+        List<Aluno> listaAlunoIds
+){
+}
---
+++ src/main/java/com/ctw/escola/dto/turma/TurmaResponseDto.java
@@ -0,0 +1,14 @@
+package com.ctw.escola.dto.turma;
+
+import com.ctw.escola.model.Aluno;
+
+import java.util.List;
+
+public record TurmaResponseDto (
+        long id,
+        String nome,
+        String nome_curso,
+        String nome_professor,
+        List<Aluno> listaAlunos
+) {
+}
