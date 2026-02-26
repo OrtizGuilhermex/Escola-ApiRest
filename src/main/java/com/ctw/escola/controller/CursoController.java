aa0cc7a59765dda8fe0f5a5db421b63420d2c2de
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 20:06:17 -0300

    feat: adiciona CursoController

---
+++ src/main/java/com/ctw/escola/controller/CursoController.java
@@ -0,0 +1,76 @@
+package com.ctw.escola.controller;
+
+import com.ctw.escola.dto.curso.CursoRequestDto;
+import com.ctw.escola.dto.curso.CursoResponseDto;
+import com.ctw.escola.service.CursoService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/cursos")
+public class CursoController {
+
+    private final CursoService cursoService;
+
+
+    public CursoController(CursoService cursoService) {
+        this.cursoService = cursoService;
+    }
+
+    @PostMapping
+    public CursoResponseDto postCurso(
+            @RequestBody CursoRequestDto cursoRequestDto
+    ){
+        try {
+            return cursoService.postCurso(cursoRequestDto);
+        }catch (SQLException e){
+            throw  new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public List<CursoResponseDto> getCursos(){
+        try{
+            return cursoService.getCursos();
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public CursoResponseDto getCursoPorId(
+            @PathVariable long id
+    ){
+        try {
+            return cursoService.getCursoPorId(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public CursoResponseDto putCurso(
+            @RequestBody CursoRequestDto cursoRequestDto,
+            @PathVariable long id
+    ){
+        try {
+            return cursoService.putCurso(cursoRequestDto,id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCurso(
+            @PathVariable long id
+    ){
+        try {
+            cursoService.deleteCurso(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
