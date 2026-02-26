913dc39b107ba2b818baef2ec3dcf1dcb09ad400
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 15:58:59 -0300

    feat: Adiciona ProfessorController

---
+++ src/main/java/com/ctw/escola/controller/ProfessorController.java
@@ -0,0 +1,73 @@
+package com.ctw.escola.controller;
+
+import com.ctw.escola.dto.professor.ProfessorRequestDto;
+import com.ctw.escola.dto.professor.ProfessorResponseDto;
+import com.ctw.escola.service.ProfessorService;
+import org.springframework.web.bind.annotation.*;
+
+import java.sql.SQLException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/professores")
+public class ProfessorController {
+
+    private  final ProfessorService professorService;
+
+    public ProfessorController(ProfessorService professorService) {
+        this.professorService = professorService;
+    }
+
+    @PostMapping
+    public ProfessorResponseDto postProfessor(
+            @RequestBody ProfessorRequestDto professorRequestDto
+    ){
+        try {
+            return professorService.postProfessor(professorRequestDto);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping
+    public List<ProfessorResponseDto> getProfessores(){
+        try {
+            return professorService.getProfessores();
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ProfessorResponseDto getProfessorPorId(long id){
+        try{
+            return professorService.getProfessorPorid(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{id}")
+    public ProfessorResponseDto putProfessor(
+            @RequestBody ProfessorRequestDto professorRequestDto,
+            @PathVariable long id
+    ){
+        try {
+            return professorService.putProfessor(professorRequestDto,id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteProfessor(
+            @PathVariable long id
+    ){
+        try {
+            professorService.deleteProfessor(id);
+        }catch (SQLException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
+}
