ff19a8cbce4b18f05977d769bbcd2892f19fc48c
luiz_gf_ortiz <luiz_gf_ortiz@estudante.sesisenai.org.br>
2026-02-26 14:04:46 -0300

    feat: Adiciona entidade Aluno

---
+++ src/main/java/com/ctw/escola/model/Aluno.java
@@ -0,0 +1,70 @@
+package com.ctw.escola.model;
+
+import java.time.LocalDate;
+
+public class Aluno {
+
+    private long id;
+    private String nome;
+    private String email;
+    private String matricula;
+    private LocalDate data_nacismento;
+
+
+    public Aluno(long id, String nome, String email, String matricula, LocalDate data_nacismento) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+        this.data_nacismento = data_nacismento;
+    }
+
+    public Aluno(String nome, String email, String matricula, LocalDate data_nacismento) {
+        this.nome = nome;
+        this.email = email;
+        this.matricula = matricula;
+        this.data_nacismento = data_nacismento;
+    }
+
+    public Aluno() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public LocalDate getData_nacismento() {
+        return data_nacismento;
+    }
+
+    public void setData_nacismento(LocalDate data_nacismento) {
+        this.data_nacismento = data_nacismento;
+    }
+}
